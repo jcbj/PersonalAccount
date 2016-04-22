804f8460769e2b74d8f3ed353249bddb148082f3
jcbj <jiangchaobj@yahoo.com>
2016-04-22 10:55:16 +0800

    添加 Summary 和 Account 编辑界面

---
+++ app/src/main/java/com/example/jc/personalaccount/EditSummaryItemActivity.java
@@ -0,0 +1,13 @@
+package com.example.jc.personalaccount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditSummaryItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_summary_item);
+    }
+}
